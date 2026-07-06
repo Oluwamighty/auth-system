e2883c8fb66385580b3161e7fce1f68a6ba8b0c7
Olawale <olawaleojo42@gmail.com>
2026-07-06 21:02:41 +0100

    Fix-use middleware export

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,5 @@
 export { auth as middleware } from "./auth"
-export default function middleware(){
 
-}
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
